f397af31de79b1d02b547a3423b46fc42f2680cf
srinivas <srinivas.panuganti56@gmail.com>
2019-10-07 11:45:48 +0530

    Update app.service.ts

--- app.service.ts
+++ app.service.ts
@@ -6,23 +6,37 @@ import{ Injectable} from '@angular/core';
 export class userService{
 
  public usersList:Array<{name:string, password:string}> =[{name:"srinivas", password:"panuganti"},
+ {name:"sainath", password:"salla"},
+ {name:"sainath", password:"salla"},
           {name:"sri",password:"hi"}]
+
+        failure:boolean=false;
+        success:boolean = false;
 
     getUsers(name:string, pwd:string){
            
      for(let i=0; i<this.usersList.length;i++){
-             
+              console.log(this.usersList.length);
             if( name == this.usersList[i].name && pwd == this.usersList[i].password){
-                        // console.log("welcome"+name);
-                        return true;
+                        console.log("welcome"+name);
+                      this.success = true;
             }
 
             else{
-                 //console.log("Please register");
-                 return false;
-
+                this.failure = false;
             }
+
    }
+     
+
+     if(this.success){
+      return true
+     }
+    else{
+        console.log(" Please register")
+        return false;
+    }
+
    }
   
   
